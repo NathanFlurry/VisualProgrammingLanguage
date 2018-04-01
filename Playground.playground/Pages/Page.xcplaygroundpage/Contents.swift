4e7c7f398e27cab17f03decd1a56e78321561146
Nathan Flurry <github@nathanflurry.com>
2018-03-31 21:53:13 -0700

    Removed old playground

--- Playground.playground/Pages/Page.xcplaygroundpage/Contents.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-import PlaygroundSupport
-
-// Create canvas view controller
-let canvasViewController = CanvasViewController()
-
-let image = UIImage(named: "background.png")
-
-PlaygroundPage.current.liveView = canvasViewController.view
