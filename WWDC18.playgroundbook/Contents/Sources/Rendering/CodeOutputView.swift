183613815ff8631811782dcadec2eccbf2b32993
Nathan Flurry <github@nathanflurry.com>
2018-04-01 08:48:17 -0700

    Fixed syntax highlighting Started essays

--- WWDC18.playgroundbook/Contents/Sources/Rendering/CodeOutputView.swift
+++ WWDC18.playgroundbook/Contents/Sources/Rendering/CodeOutputView.swift
@@ -22,7 +22,7 @@ class CodeOutputView: UIView {
             "willSet"
     ]
     
-    let splitCharacters: [String] = [ " ", "{", "}", "(", ")", "[", "]" ]
+    let splitCharacters: [String] = [ "\n", " ", "{", "}", "(", ")", "[", "]" ]
     
     var code: String = ""
     
